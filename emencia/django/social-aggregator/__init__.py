bdccbfcf9468e82a18dc9aa13ef986d1c3affee2
Lafaye Philippe <lafaye@emencia.com>
2013-12-10 17:12:16 +0100

    add app structure

---
+++ emencia/django/social-aggregator/__init__.py
@@ -0,0 +1,8 @@
+"""emencia.django.social-aggregator"""
+__version__ = '0.1.dev'
+__license__ = 'AGPL License'
+
+__author__ = 'rage2000'
+__email__ = 'lafaye@emencia.com'
+
+__url__ = 'http://emencia.fr/'
