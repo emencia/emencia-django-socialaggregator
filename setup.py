0ee74a9261296cb3e2a5dfa50769366e5c12b4b8
David THENON <sveetch@gmail.com>
2014-02-05 16:37:29 +0100

    Fix setup.py for a working package

--- setup.py
+++ setup.py
@@ -1,34 +1,38 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
-
-setup(name='emencia-django-socialaggregator',
-      version=__import__('socialaggregator').__version__,
-      description='Django app for aggregate some feeds from social networks.',
-      long_description="%s\n%s" % (open('README.rst').read(),
-                       open(os.path.join('docs', 'changelog.rst')).read()),
-      keywords='django, emencia, social networks, aggregation',
-      classifiers=[
-          'Framework :: Django',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Environment :: Web Environment',
-          'Intended Audience :: Developers',
-          'Operating System :: OS Independent',
-          'License :: OSI Approved :: GNU Affero General Public License v3',
-          'Development Status :: 2 - Pre-Alpha',
-          'Topic :: Software Development :: Libraries :: Python Modules'],
-      author=__import__('socialaggregator').__author__,
-      author_email=__import__('socialaggregator').__email__,
-      url=__import__('socialaggregator').__url__,
-      license=__import__('socialaggregator').__license__,
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=['setuptools',
-                        'django-taggit',
-                        'twitter',
-                        'python-instagram',
-                        'facebook-sdk',
-                        'feedparser',
-                        'google-api-python-client',
-                        'django'])
+setup(
+    name='emencia-django-socialaggregator',
+    version=__import__('socialaggregator').__version__,
+    description='Django app for aggregate some feeds from social networks.',
+    long_description="%s\n%s" % (open('README.rst').read(), open(os.path.join('docs', 'changelog.rst')).read()),
+    keywords='django, emencia, social networks, aggregation',
+    author=__import__('socialaggregator').__author__,
+    author_email=__import__('socialaggregator').__email__,
+    url=__import__('socialaggregator').__url__,
+    license=__import__('socialaggregator').__license__,
+    packages=find_packages(),
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    install_requires=[
+        'setuptools',
+        'django-taggit',
+        'twitter',
+        'python-instagram',
+        'facebook-sdk',
+        'feedparser',
+        'google-api-python-client',
+        'django'
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
